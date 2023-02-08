528613cc5fe993440d88702dcdd00fdfcfd46697
alekon94 <alekon43@gmail.com>
2023-02-08 12:24:11 +0300

    ADDED: 404 page, components for Products

--- src/Pages/products/index.js
+++ src/Pages/products/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Id from './product';
+
+const List = React.lazy(() => import('./list'));
+const Id = React.lazy(() => import('./product'));
 
 export default function Products() {
     return (
         <Routes>
+            <Route path="/" element={<List />} />
             <Route path="/:id" element={<Id />} />
         </Routes>
     );
--- src/Pages/products/list/index.js
+++ src/Pages/products/list/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function ProductsList() {
+export default function List() {
     return <p>Products List</p>;
 }
