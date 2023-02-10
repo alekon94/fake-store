cf1304c734e3c1c23955158503bc4de1cc1cd29e
alekon94 <alekon43@gmail.com>
2023-02-10 18:21:19 +0300

    Added: propertyResponsive and mediaBreakpointUp mixins, breakpoints, sizes, keep work with header

--- src/theme/mixins/loadFontFamily.js
+++ src/theme/mixins/loadFontFamily.js
@@ -1,20 +1,3 @@
-// import { css } from 'styled-components';
-
-// const loadFontFamily = ({ name, path, weight, style }) => {
-//     if (name && path) {
-//         return css`
-//             @font-face {
-//                 font-family: ${name};
-//                 src: url('{$path}.woff2') format('woff2'),
-//                     url('#{$path}.woff') format('woff');
-//                 font-weight: ${weight||'normal'};
-//                 font-style: ${style||'normal'};
-//                 font-display: swap;
-//             }
-//         `
-//     }
-//     return null;
-// };
 const loadFontFamily = ({ name, woff, woff2, style, weight }) => `
   @font-face {
     font-family: "${name}";
